b86149bd10cef415965f69b134cd5ae1b472a4ba
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-14 15:43:53 +0100

    functions outside the eventlistener

--- script2.js
+++ script2.js
@@ -1,7 +1,33 @@
 
 // Prendo gli elementi dal dom 
 const winnerBtn = document.getElementById("disc-winner");
-const displayElement = document.getElementById("result-display");
+const displayElement1 = document.getElementById("result-display");
+
+
+
+// Costruisco una funzione per generare un numero random da 1 a 5 in questo caso 
+function rndNum(min , max){
+    max ++;
+    return Math.floor(Math.random() *  (max - min)) + min;
+}
+
+// Costruisco una funzione per calcolare la somma tra il numero random e quello dell'utente 
+function sum(num1 , num2){
+    const somma = num1 + num2;
+    return somma;
+}
+
+// Costruisco una funzione per verificare se la somma è pari o dispari 
+function isOdd(total){
+    let result = "";
+    if(total % 2){
+        result = "dispari";
+    }else {
+        result = "pari";
+    }
+    return result;   
+}   
+
 
 
 // Associo al bottone l'evento click 
@@ -11,29 +37,6 @@ winnerBtn.addEventListener('click' , function (){
     const userChoice = document.getElementById("user-choice").value.trim();
     const userNumber = parseInt(document.getElementById("user-number").value.trim());
 
-    // Costruisco una funzione per generare un numero random da 1 a 5 in questo caso 
-    function rndNum(min , max){
-        max ++;
-        return Math.floor(Math.random() *  (max - min)) + min;
-    }
-    
-    // Costruisco una funzione per calcolare la somma tra il numero random e quello dell'utente 
-    function sum(num1 , num2){
-        const somma = num1 + num2;
-        return somma;
-    }
-    
-    // Costruisco una funzione per verificare se la somma è pari o dispari 
-    function isOdd(total){
-        let result = "";
-        if(total % 2){
-            result = "dispari";
-        }else {
-            result = "pari";
-        }
-        return result;
-        
-    }    
     
     // Invoco le funzioni con i relativi paramentri e stampo il risultato in console 
     const pcRandom = rndNum(1 , 5);
@@ -47,9 +50,9 @@ winnerBtn.addEventListener('click' , function (){
     
     // Controllo se ha vinto l'utente con la scelta di prima o il computer 
     if(userChoice === pariDispari){
-        displayElement.innerText = "Ha vinto l'utente";
+        displayElement1.innerText = "Ha vinto l'utente";
     } else{
-        displayElement.innerText = "Ha vinto il computer";
+        displayElement1.innerText = "Ha vinto il computer";
     }
 });
 
