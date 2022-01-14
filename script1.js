d48d511999ca72d1b2affe0294cb750a010542d2
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-14 15:09:42 +0100

    comments added

--- script1.js
+++ script1.js
@@ -1,26 +1,37 @@
+
+// Prendo gli elementi dal dom 
 const palBtn = document.getElementById("disc-pal");
 const displayElement = document.getElementById("result-display");
 
+
+// Collego il tutto all'evento click sul bottone 
 palBtn.addEventListener('click' , function (){
+
+    // Prendo il valore dall'input 
     const inputWord = document.getElementById("user-word").value;
     
+    // Costruisco una funzione per montare la parola al contrario e confrontarla con l'originale
     function isPal  (userWord){
         let reversedWord = "";
           
+        // Faccio passare tutti i caratteri della stringa dell'utente al contrario e li metto nella parola reverse 
         for (let i = userWord.length - 1; i >= 0; i--){
         reversedWord += userWord[i];
         console.log(userWord[i]);
         }
         
+        // Controllo se la parola al contrario è uguale alla parola originale 
         if(reversedWord === userWord){
                 return true;
             }
             return false;
         }
-            
+          
+        // Invoco la funzione per avere il risultato booleano 
         const isPalindrome = isPal(inputWord);
         console.log(isPalindrome);
         
+        // Stampo in pagina il risultato
         if (isPalindrome){
             displayElement.innerText = "La parola è Palindroma";
         } else{
--- script2.js
+++ script2.js
@@ -1,21 +1,29 @@
+
+// Prendo gli elementi dal dom 
 const winnerBtn = document.getElementById("disc-winner");
 const displayElement = document.getElementById("result-display");
 
+
+// Associo al bottone l'evento click 
 winnerBtn.addEventListener('click' , function (){
+
+    // Prendo il valore dei campi input dal dom 
     const userChoice = document.getElementById("user-number").value;
     const userNumber = parseInt(document.getElementById("user-choice").value);
 
-    
+    // Costruisco una funzione per generare un numero random da 1 a 5 in questo caso 
     function rndNum(min , max){
         max ++;
         return Math.floor(Math.random() *  (max - min)) + min;
     }
     
+    // Costruisco una funzione per calcolare la somma tra il numero random e quello dell'utente 
     function sum(num1 , num2){
         const somma = num1 + num2;
         return somma;
     }
     
+    // Costruisco una funzione per verificare se la somma è pari o dispari 
     function isOdd(total){
         let result = "";
         if(total % 2){
@@ -27,6 +35,7 @@ winnerBtn.addEventListener('click' , function (){
         
     }    
     
+    // Invoco le funzioni con i relativi paramentri e stampo il risultato in console 
     const pcRandom = rndNum(1 , 5);
     console.log(`Il numero del pc è ${pcRandom}`);
     
@@ -36,6 +45,7 @@ winnerBtn.addEventListener('click' , function (){
     const pariDispari = isOdd(sommaNumeri);
     console.log(pariDispari);
     
+    // Controllo se ha vinto l'utente con la scelta di prima o il computer 
     if(userChoice === pariDispari){
         displayElement.innerText = "Ha vinto l'utente";
     } else{
