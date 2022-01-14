890aa00cd1bb89ce59d16f2d5c8face68ae4a33c
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-14 13:57:24 +0100

    upload files

---
+++ script1.js
@@ -0,0 +1,11 @@
+
+function isPal (reversedWord , userWord){
+   let reversedWord = "";
+   for (let i = reversedWord.length - 1; i >= 0; i++){
+       reversedWord += userWord[i];
+   }
+
+   for (let i = userWord.length - 1; i >= 0; i++){
+       if(userWord[i] === )
+   }
+}
