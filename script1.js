70f8c65e56b5174ac76249b1b8547f65f24c70eb
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-14 14:20:01 +0100

    first exercise

--- script1.js
+++ script1.js
@@ -1,11 +1,28 @@
 
-function isPal (reversedWord , userWord){
-   let reversedWord = "";
-   for (let i = reversedWord.length - 1; i >= 0; i++){
-       reversedWord += userWord[i];
-   }
+const inputWord = prompt("inserisci una parola");
 
-   for (let i = userWord.length - 1; i >= 0; i++){
-       if(userWord[i] === )
-   }
-}
+function isPal  (userWord){
+    let reversedWord = "";
+      
+    for (let i = userWord.length - 1; i >= 0; i--){
+    reversedWord += userWord[i];
+    console.log(userWord[i]);
+    }
+    
+    if(reversedWord === userWord){
+            return true;
+        }
+        return false;
+    }
+        
+    const isPalindrome = isPal(inputWord);
+    console.log(isPalindrome);
+    
+    if (isPalindrome){
+        console.log("la parola è palindroma");
+    } else{
+        console.log("la parola non è palindroma");
+    }
+        
+                
+                
