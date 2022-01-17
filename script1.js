b07027993388c3dae2dbc31d9c3de42fd25084e2
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-17 10:19:20 +0100

    used create element appendchild and append methods

--- script1.js
+++ script1.js
@@ -7,48 +7,51 @@ const displayElement = document.getElementById("result-display-1");
 
 
 // Costruisco una funzione per montare la parola al contrario e confrontarla con l'originale
-function isPal  (userWord){
+function isPal(userWord) {
     let reversedWord = "";
-      
+
     // Faccio passare tutti i caratteri della stringa dell'utente al contrario e li metto nella parola reverse 
-    for (let i = userWord.length - 1; i >= 0; i--){
-    reversedWord += userWord[i];
-    console.log(userWord[i]);
+    for (let i = userWord.length - 1; i >= 0; i--) {
+        reversedWord += userWord[i];
+        console.log(userWord[i]);
     }
-    
+
     // Controllo se la parola al contrario è uguale alla parola originale 
-    if(reversedWord === userWord){
-            return true;
-        }
-        return false;
+    if (reversedWord === userWord) {
+        return true;
+    }
+    return false;
 }
 
 
 
 
 // Collego il tutto all'evento click sul bottone 
-palBtn.addEventListener('click' , function (){
+palBtn.addEventListener('click', function () {
 
     // Prendo il valore dall'input 
     const inputWord = document.getElementById("user-word").value.trim();
 
-       
-        if(parseInt(inputWord) == inputWord || inputWord.length == 0){
-            displayElement.innerText = "La parola che hai inserito non è valida";
+
+    if (parseInt(inputWord) == inputWord || inputWord.length == 0) {
+        displayElement.innerText = "La parola che hai inserito non è valida";
+    } else {
+
+
+        // Invoco la funzione per avere il risultato booleano 
+        const isPalindrome = isPal(inputWord);
+        console.log(isPalindrome);
+
+        // Stampo in pagina il risultato
+        if (isPalindrome) {
+            const newElement = document.createElement('div');
+            displayElement.appendChild(newElement);
+            newElement.append("La parola è palindroma");
         } else {
-        
-            
-            // Invoco la funzione per avere il risultato booleano 
-            const isPalindrome = isPal(inputWord);
-            console.log(isPalindrome);
-            
-            // Stampo in pagina il risultato
-            if (isPalindrome){
-                displayElement.innerText = "La parola è Palindroma";
-            } else{
-                displayElement.innerText = "La parola non è Palindroma";
-            }
+            const newElement = document.createElement('div');
+            displayElement.appendChild(newElement);
+            newElement.append("La parola non è palindroma");
+        }
     }
 
 });
-                
--- script2.js
+++ script2.js
@@ -6,56 +6,55 @@ const displayElement1 = document.getElementById("result-display-2");
 
 
 // Costruisco una funzione per generare un numero random da 1 a 5 in questo caso 
-function rndNum(min , max){
-    max ++;
-    return Math.floor(Math.random() *  (max - min)) + min;
+function rndNum(min, max) {
+    max++;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // Costruisco una funzione per calcolare la somma tra il numero random e quello dell'utente 
-function sum(num1 , num2){
+function sum(num1, num2) {
     const somma = num1 + num2;
     return somma;
 }
 
 // Costruisco una funzione per verificare se la somma è pari o dispari 
-function isOdd(total){
+function isOdd(total) {
     let result = "";
-    if(total % 2){
+    if (total % 2) {
         result = "dispari";
-    }else {
+    } else {
         result = "pari";
     }
-    return result;   
-}   
+    return result;
+}
 
 
 
 // Associo al bottone l'evento click 
-winnerBtn.addEventListener('click' , function (){
+winnerBtn.addEventListener('click', function () {
 
     // Prendo il valore dei campi input dal dom 
     const userChoice = document.getElementById("user-choice").value;
     const userNumber = parseInt(document.getElementById("user-number").value.trim());
 
-    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5){
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
         displayElement1.innerText = "Non hai inserito un numero corretto";
-    } else{
+    } else {
         // Invoco le funzioni con i relativi paramentri e stampo il risultato in console 
-        const pcRandom = rndNum(1 , 5);
+        const pcRandom = rndNum(1, 5);
         console.log(`Il numero del pc è ${pcRandom}`);
-        
-        const sommaNumeri = sum(userNumber , pcRandom);
+
+        const sommaNumeri = sum(userNumber, pcRandom);
         console.log(`la somma è ${sommaNumeri}`);
-        
+
         const pariDispari = isOdd(sommaNumeri);
         console.log(pariDispari);
-        
+
         // Controllo se ha vinto l'utente con la scelta di prima o il computer 
-        if(userChoice === pariDispari){
+        if (userChoice === pariDispari) {
             displayElement1.innerText = "Ha vinto l'utente";
-        } else{
+        } else {
             displayElement1.innerText = "Ha vinto il computer";
         }
     }
 });
-
